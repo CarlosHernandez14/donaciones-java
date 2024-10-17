66b2704124e9bd2c14680756e6bad570125fa632
CarlosHernandez14 <carloskvpchc@gmail.com>
2024-10-17 11:35:29 -0600

    First commit

---
+++ src/main/java/com/mycompany/donacionesprueba/DonacionesPrueba.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.donacionesprueba;
+
+/**
+ *
+ * @author carlo
+ */
+public class DonacionesPrueba {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
